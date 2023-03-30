ca39d55901c8a6e3e3fa7a8fdb2a1db28eaa04f9
Pradyumna Malladi <f20210367@hyderabad.bits-pilani.ac.in>
2023-03-30 18:49:32 +0530

    "Initial Commit"

---
+++ chat.py
@@ -0,0 +1,65 @@
+import openai
+import os
+import json
+import re
+
+# set your OpenAI API key
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# define the prompt for the text generation
+s2 = " Give me a valid json object with this structure: 1) 'title' field representing title of ppt 2) 'slides' field representing array of slides 3) Each 'slide' having: 'title', 'content' fields. Also, 'content' is a list of strings representing points each not having more than 20 words. Check that output is a valid json."
+req = "Make a powerpoint presentation with five slides for a programmer audience on technical and ethical advantages of linux vs windows with each slide having around 200 words. It should be technical and complex."
+prompt = req + s2
+
+
+# define the model and parameters
+model_engine = "text-davinci-002"
+parameters = {
+    "temperature": 0.5,
+    "max_tokens": 3000,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0
+}
+
+# generate text using the model and prompt
+response = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=parameters["max_tokens"],
+    temperature=parameters["temperature"],
+    top_p=parameters["top_p"],
+    frequency_penalty=parameters["frequency_penalty"],
+    presence_penalty=parameters["presence_penalty"]
+)
+
+result = response["choices"][0]["text"]
+
+output = re.sub(r'^.*?{', '{', result)
+
+# Save the JSON data string to a file
+with open("ppt.json", "w") as json_file:
+    json_file.write(output)
+
+
+with open("ppt.json", "r") as f:
+    # Read the lines of the file into a list
+    lines = f.readlines()
+
+delimiter = "{"
+for i in range(len(lines)):
+    # Find the index of the delimiter in the current line
+    index = lines[i].find(delimiter)
+    # If found....
+    if index != -1:
+        # Remove characters before the delimiter and update the line
+        lines[i] = lines[i][index:]
+        break
+    else:
+        lines[i]=""
+
+# Write the updated lines to a new file
+with open("ppt1.json", "w") as f:
+    f.writelines(lines)
+
+print(output)
